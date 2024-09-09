4527d54158d2d91d28037ccd005a8aeeae4b95f5
Taimoor Abbasi <taimoor.abbasi@monetizemore.com>
2024-09-10 00:23:51 +0500

    Initial Commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/mtaimoor1/aws-utils/s3"
+)
+
+func main() {
+	s3.MoveS3Obj("", "", "", "")
+	// fmt.Printf("Length of result is: %d\n", len(res))
+	// fmt.Printf("Length of result is: %s\n", res)
+}
---
+++ s3/s3.go
@@ -0,0 +1,77 @@
+package s3
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+func getS3Con() *s3.Client {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return s3.NewFromConfig(cfg)
+}
+
+func getListOfS3Obj(s3_client *s3.Client, bucket string, prefix string) ([]types.Object, error) {
+
+	result := []types.Object{}
+
+	list_payload := &s3.ListObjectsV2Input{
+		Bucket:  aws.String(bucket),
+		Prefix:  aws.String(prefix),
+		MaxKeys: aws.Int32(2),
+	}
+	for {
+		output, err := s3_client.ListObjectsV2(context.TODO(), list_payload)
+		if err != nil {
+			log.Fatal(err)
+			return nil, err
+		}
+
+		result = append(result, output.Contents...)
+
+		if *output.IsTruncated == true {
+			list_payload.ContinuationToken = output.NextContinuationToken
+		} else {
+			break
+		}
+	}
+	return result, nil
+}
+
+func ListS3Objs(bucket string, prefix string) {
+
+	s3_client := getS3Con()
+
+	results, err := getListOfS3Obj(s3_client, bucket, prefix)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Objects from %s are as follows:\n", bucket)
+	for _, obj := range results {
+		log.Printf("key: %s size %d date %s", *obj.Key, obj.Size, obj.LastModified)
+	}
+}
+
+func MoveS3Obj(s_bucket string, s_prefix string, d_bucket string, d_prefix string) []types.Object {
+	s3_client := getS3Con()
+	res, err := getListOfS3Obj(s3_client, s_bucket, s_prefix)
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+	for _, obj := range res {
+		s3_client.CopyObject(context.TODO(), &s3.CopyObjectInput{
+			Bucket:     aws.String(d_bucket),
+			CopySource: aws.String(s_bucket + "/" + *obj.Key),
+			Key:        aws.String(d_prefix + *obj.Key),
+		})
+	}
+	return res
+}
